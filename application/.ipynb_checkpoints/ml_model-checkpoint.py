5e135bf3a924b409d40b6b4728d0e1be0f56c2f6
ayang <ayang.laishram@gmail.com>
2024-12-29 16:31:11 +0530

    created model joblib file

---
+++ application/.ipynb_checkpoints/ml_model-checkpoint.py
@@ -0,0 +1,6 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+
+#Load the iris dataset
+iris_dataset = load_iris()
+print('------dataset-------', iris_dataset)
